c2900f80235fcfed70a4adf9dc58e6d941c40076
Kane <LisaxKane@gmail.com>
2022-11-04 02:41:09 -0500

    added an edit button with detail button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import "./style.scss";
-import ExpandArrow from './expand.svg'
-
-
+import ExpandArrow from "./expand.svg";
 
 let taskPriority = undefined;
 var projectArray = [];
@@ -9,13 +7,12 @@ let taskArray = [];
 
 class Project {
   constructor(projectName) {
-  this.ProjectName = projectName;
+    this.ProjectName = projectName;
   }
   pushProject() {
-    projectArray.push(this.ProjectName)
+    projectArray.push(this.ProjectName);
   }
 }
-
 
 class Task {
   constructor(title, priority, dueDate, description) {
@@ -32,35 +29,27 @@ class Task {
   }
 }
 
-
-
-
 const closeBtn = document.querySelector(".close");
 const addProject = document.querySelector(".addProject");
 const bgModal = document.querySelector(".bg-modal");
 const lowPrio = document.querySelector(".LP");
 const medPrio = document.querySelector(".MP");
 const HighPrio = document.querySelector(".HP");
-const AppendBtn = document.querySelector('.submitProject');
-const middle = document.querySelector('.middle');
+const AppendBtn = document.querySelector(".submitProject");
+const middle = document.querySelector(".middle");
 
-AppendBtn.addEventListener('click', function () {
+AppendBtn.addEventListener("click", function () {
   appendProject();
-})
-
+});
 
 addProject.addEventListener("click", function () {
-  this.classList.toggle('slide')
-  document.querySelector('.ProjectInput').style.display = 'flex';
-  document.querySelector('.btnCont').style.display = 'flex';
-  if (this.classList.value == 'addProject')
-  {
-    
-    
-    document.querySelector('.ProjectInput').style.display = 'none';
-    document.querySelector('.btnCont').style.display = 'none';
-    }
-  
+  this.classList.toggle("slide");
+  document.querySelector(".ProjectInput").style.display = "flex";
+  document.querySelector(".btnCont").style.display = "flex";
+  if (this.classList.value == "addProject") {
+    document.querySelector(".ProjectInput").style.display = "none";
+    document.querySelector(".btnCont").style.display = "none";
+  }
 });
 
 closeBtn.addEventListener("click", function () {
@@ -86,17 +75,11 @@ HighPrio.addEventListener("click", function () {
   medPrio.style.backgroundColor = "rgb(230, 248, 255)";
 });
 
-
-
-
-
-
 function AddTask() {
   if (taskPriority == undefined) {
     alert("please choose a priority");
     return;
   }
-
 
   let taskName = document.querySelector(".taskName").value;
   let description = document.querySelector(".description").value;
@@ -111,57 +94,84 @@ function appendProject() {
   var project = new Project(pname);
 
   project.pushProject();
- 
 
   makeProjectDiv();
-  console.log(projectArray)
+  console.log(projectArray);
 }
-
 
 function makeProjectDiv() {
   const ArrowPic = new Image();
   ArrowPic.src = ExpandArrow;
+  const right = document.querySelector(".right");
+  const tab = document.createElement("tabDiv");
+  const detail = document.createElement("detail");
+  const projDiv = document.createElement("div");
+  const tabBtnContainer = document.createElement("tabBtnContainer");
+  let addTaskBtn = document.createElement("taskBtn");
+  const editBtn = document.createElement("edit");
+  const closeBtn = document.createElement("closeBtn");
+  closeBtn.textContent = "+";
 
-  const projDiv = document.createElement('div');
-  const addTaskBtn = document.createElement('taskBtn');
-  const editBtn = document.createElement('edit')
-  const closeBtn = document.createElement('closeBtn')
-  closeBtn.textContent = '+'
-  addTaskBtn.textContent = 'Add Task'
-  
-  projDiv.classList.add('Project')
-  projDiv.setAttribute('id', projectArray.length)
-  projDiv.textContent = projectArray.slice(-1)
-  
+  projDiv.classList.add("Project");
+  projDiv.setAttribute("id", projectArray.length);
+  projDiv.textContent = projectArray.slice(-1);
+
   middle.appendChild(projDiv);
 
-  projDiv.appendChild(addTaskBtn);
-  projDiv.appendChild(editBtn);
+  projDiv.appendChild(tab);
+  tabBtnContainer.appendChild(detail);
+  tab.appendChild(tabBtnContainer);
+  tabBtnContainer.appendChild(addTaskBtn);
+  tabBtnContainer.appendChild(editBtn);
+
   projDiv.appendChild(closeBtn);
   projDiv.appendChild(ArrowPic);
-
-  
-  
-
 }
 
-
-document.querySelector('body').addEventListener('click', function (event) {
-    if (event.target.tagName.toLowerCase() === 'img') {
-      
-    
-      event.target.classList.toggle('rotate');
-      console.log('hi')
+function makeTaskDiv() {
+  document.querySelector("body").addEventListener("click", function (event) {
+    if (event.target.classList == "test") {
+      console.log("ye");
+      const testtask = document.createElement("task");
+      testtask.setAttribute("id", "Tasknum");
+      document.querySelector("tabDiv").appendChild(testtask);
     }
-})
+  });
+}
 
-document.querySelector('body').addEventListener('click', function (event) {
-  if (event.target.tagName.toLowerCase() == 'closebtn')
-  {
+document.querySelector("body").addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() === "img") {
+    event.target.parentElement.querySelector("taskBtn").style.display = "none";
+    event.target.parentElement.querySelector("edit").style.display = "none";
+    event.target.parentElement.querySelector("detail").style.display = "none";
+
+    event.target.classList.toggle("rotate");
+    event.target.parentElement
+      .querySelector("tabDiv")
+      .classList.toggle("scrollSlide");
+  }
+  if (event.target.classList == "rotate") {
+    event.target.parentElement.querySelector("taskBtn").style.display = "flex";
+    event.target.parentElement.querySelector("taskBtn").textContent =
+      "Add Task";
+    event.target.parentElement.querySelector("edit").style.display = "flex";
+    event.target.parentElement.querySelector("edit").textContent = "Edit";
+    event.target.parentElement.querySelector("detail").style.display = "flex";
+    event.target.parentElement.querySelector("detail").textContent = "detail";
+  }
+
+  if (event.target.tagName.toLowerCase() === "taskbtn") {
+    document.querySelector(".bg-modal").style.display = "flex";
+  }
+});
+
+document.querySelector("body").addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() == "closebtn") {
     event.target.parentElement.remove();
-    }
-})
+  }
+});
 
-document.querySelector(".test").addEventListener("click", AddTask);
-
-console.log(taskArray);
+document.querySelector(".test").addEventListener("click", function () {
+  AddTask();
+  makeTaskDiv();
+});
