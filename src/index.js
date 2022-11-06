c678b174807af514a9faaaf008ba2c5200d9efbb
Kane <LisaxKane@gmail.com>
2022-11-06 01:33:14 -0600

    give up for today, still need edit button to work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./style.scss";
 import ExpandArrow from "./expand.svg";
+
 var taskProject;
 let taskPriority = undefined;
 var projectArray = [];
-let taskArray = [];
-
+var taskArray = [];
+var nav = "#10274e";
+console.log(taskArray.length);
 class Project {
   constructor(projectName) {
     this.ProjectName = projectName;
@@ -15,13 +17,14 @@ class Project {
 }
 
 class Task {
-  constructor(title, priority, dueDate, description, project) {
+  constructor(title, priority, dueDate, description, project, show) {
     this.title = title;
     this.dueDate = dueDate;
     this.priority = priority;
     this.description = description;
     this.project = project;
     this.finish = false;
+    this.show = show;
   }
   Task2List() {
     taskArray.push([
@@ -30,6 +33,7 @@ class Task {
       this.dueDate,
       this.description,
       this.project,
+      this.show,
     ]);
     console.log(taskArray);
     return this;
@@ -66,29 +70,60 @@ closeBtn.addEventListener("click", function () {
 lowPrio.addEventListener("click", function () {
   taskPriority = "Low";
   this.style.backgroundColor = "green";
-  medPrio.style.backgroundColor = "rgb(230, 248, 255)";
-  HighPrio.style.backgroundColor = "rgb(230, 248, 255)";
+  medPrio.style.backgroundColor = nav;
+  HighPrio.style.backgroundColor = nav;
 });
 
 medPrio.addEventListener("click", function () {
   taskPriority = "Medium";
   this.style.backgroundColor = "yellow";
-  lowPrio.style.backgroundColor = "rgb(230, 248, 255)";
-  HighPrio.style.backgroundColor = "rgb(230, 248, 255)";
+  lowPrio.style.backgroundColor = nav;
+  HighPrio.style.backgroundColor = nav;
 });
 
 HighPrio.addEventListener("click", function () {
   taskPriority = "High";
   this.style.backgroundColor = "red";
-  lowPrio.style.backgroundColor = "rgb(230, 248, 255)";
-  medPrio.style.backgroundColor = "rgb(230, 248, 255)";
+  lowPrio.style.backgroundColor = nav;
+  medPrio.style.backgroundColor = nav;
 });
 
 // listen to body for click on tagnam img
 document.querySelector("body").addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() === "edit") {
+    var currentid = parseInt(event.target.parentElement.parentElement.id);
+    console.log(currentid);
+    console.log(event.target.parentElement.parentElement.id);
+    document
+      .querySelector(".edit-test")
+      .setAttribute("id", event.target.parentElement.parentElement.id);
+
+    document.querySelector(".edit-taskName").textContent =
+      taskArray[event.target.parentElement.parentElement.id][0];
+
+    document.querySelector(".edit-description").textContent =
+      taskArray[event.target.parentElement.parentElement.id][3];
+    document.querySelector(".edit-taskDue").textContent =
+      taskArray[event.target.parentElement.parentElement.id][2];
+    document.querySelector(".edit-bg-modal").style.display = "flex";
+  }
+  if (event.target.classList == "edit-test") {
+    if (taskPriority == undefined) {
+      alert("please choose a priority");
+      return;
+    }
+
+    console.log(taskArray);
+  }
+  if (event.target.tagName.toLowerCase() === "remove") {
+    console.log(taskArray.length);
+    event.target.parentElement.parentElement.remove();
+    console.log(event.target.parentElement.parentElement);
+    taskArray[event.target.parentElement.parentElement.id][5] = "false";
+  }
   if (event.target.tagName.toLowerCase() === "img") {
     event.target.parentElement.querySelector("taskBtn").style.display = "none";
-    event.target.parentElement.querySelector("edit").style.display = "none";
+
     event.target.parentElement.querySelector("detail").style.display = "none";
 
     event.target.classList.toggle("rotate");
@@ -101,14 +136,12 @@ document.querySelector("body").addEventListener("click", function (event) {
   }
   if (event.target.tagName.toLowerCase() === "taskbtn") {
     taskProject = event.target.id;
-    console.log(event.target.id);
   }
   if (event.target.classList == "rotate") {
     event.target.parentElement.querySelector("taskBtn").style.display = "flex";
     event.target.parentElement.querySelector("taskBtn").textContent =
       "Add Task";
-    event.target.parentElement.querySelector("edit").style.display = "flex";
-    event.target.parentElement.querySelector("edit").textContent = "Edit";
+
     event.target.parentElement.querySelector("detail").style.display = "flex";
     event.target.parentElement.querySelector("detail").textContent = "detail";
   }
@@ -123,42 +156,54 @@ document.querySelector("body").addEventListener("click", function (event) {
     document.querySelector(".detailModal").style.display = "flex";
 
     let tempArr = [];
+
     const realtaskList = document.querySelector(".realtaskList");
     removeAllChild(realtaskList);
     for (let i = 0; i < taskArray.length; i++) {
-      if (taskArray[i][4] == event.target.id) {
-        tempArr.push(taskArray[i]);
+      if (taskArray[i][4] === undefined || taskArray[i] === undefined) {
+        return;
       }
+      if (taskArray[i][4] == event.target.id && taskArray[i][5] == "true") {
+        tempArr.push(taskArray[i]);
 
-      if (taskArray[i][4] == event.target.id) {
-        var listOfTask = document.createElement("divfortask");
-        listOfTask.setAttribute("id", event.target.id);
-        const taskTitle = document.createElement("taskTitle");
-        const taskDes = document.createElement("taskDes");
-        const taskDD = document.createElement("taskDD");
-        const taskPrio = document.createElement("taskPrio");
-        taskDes.textContent = tempArr.slice(-1)[0][3];
-        taskTitle.textContent = tempArr.slice(-1)[0][0];
-        taskDD.textContent = tempArr.slice(-1)[0][2];
-        taskPrio.textContent = tempArr.slice(-1)[0][1];
-        listOfTask.appendChild(taskTitle);
+        if (taskArray[i][4] == event.target.id) {
+          var listOfTask = document.createElement("divfortask");
+          listOfTask.setAttribute("id", i);
+          const taskTitle = document.createElement("taskTitle");
+          const taskDes = document.createElement("taskDes");
+          const taskDD = document.createElement("taskDD");
+          const taskPrio = document.createElement("taskPrio");
+          const editBtn = document.createElement("edit");
+          const RmBtn = document.createElement("remove");
+          const editRmBtnContainer = document.createElement("thosebtns");
+          editBtn.textContent = "Edit";
+          RmBtn.textContent = "Remove";
+          taskDes.textContent = tempArr.slice(-1)[0][3];
+          taskTitle.textContent = tempArr.slice(-1)[0][0];
+          taskDD.textContent = tempArr.slice(-1)[0][2];
+          taskPrio.textContent = tempArr.slice(-1)[0][1];
+          listOfTask.appendChild(taskTitle);
 
-        listOfTask.appendChild(taskDes);
-        listOfTask.appendChild(taskDD);
-        listOfTask.appendChild(taskPrio);
-        realtaskList.appendChild(listOfTask);
+          listOfTask.appendChild(taskDes);
+          listOfTask.appendChild(taskDD);
+          listOfTask.appendChild(taskPrio);
+          listOfTask.appendChild(editRmBtnContainer);
+          editRmBtnContainer.appendChild(editBtn);
+          editRmBtnContainer.appendChild(RmBtn);
+          realtaskList.appendChild(listOfTask);
+        }
       }
     }
   }
 
   if (event.target.classList == "detailClose") {
-    console.log("hi");
     document.querySelector(".detailModal").style.display = "none";
   }
 });
 
 document.querySelector(".test").addEventListener("click", function () {
   AddTask();
+  console.log(taskArray.length);
 });
 
 //function that removes all child from parent
@@ -180,7 +225,7 @@ function makeProjectDiv() {
   let addTaskBtn = document.createElement("taskBtn");
   addTaskBtn.setAttribute("id", projectArray.slice(-1));
   detail.setAttribute("id", projectArray.slice(-1));
-  const editBtn = document.createElement("edit");
+
   const closeBtn = document.createElement("closeBtn");
   closeBtn.textContent = "+";
 
@@ -194,7 +239,6 @@ function makeProjectDiv() {
   tabBtnContainer.appendChild(detail);
   tab.appendChild(tabBtnContainer);
   tabBtnContainer.appendChild(addTaskBtn);
-  tabBtnContainer.appendChild(editBtn);
 
   projDiv.appendChild(closeBtn);
   projDiv.appendChild(ArrowPic);
@@ -219,12 +263,14 @@ function AddTask() {
   let taskName = document.querySelector(".taskName").value;
   let description = document.querySelector(".description").value;
   let taskDue = document.querySelector(".taskDue").value;
+  let show = "true";
   var tasks = new Task(
     taskName,
     taskPriority,
     taskDue,
     description,
-    taskProject
+    taskProject,
+    show
   );
   tasks.Task2List();
   document.querySelector(".bg-modal").style.display = "none";
